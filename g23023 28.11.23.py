4908f262f501927a3cbca46faf725b57539906d1
kolbosiator <146109643+kolbosiator@users.noreply.github.com>
2023-11-28 17:36:41 +0300

    g23023 28.11.23

---
+++ g23023 28.11.23.py
@@ -0,0 +1,104 @@
+#bol1
+
+##a=int(input())
+##res = a > 0
+##print(res)
+
+#bol2
+
+##b=int(input())
+##res = b % 2 > 0
+##print(res)
+
+#bol3
+
+##b=int(input())
+##res = b % 2 == 0
+##print(res)
+
+#bol4
+
+##a=int(input())
+##b=int(input())
+##res = a > 2 
+##res1 = b <= 3
+##print(res)
+##print(res1)
+
+#bol5
+
+##a=int(input())
+##b=int(input())
+##res = a >= 0 
+##res1 = b < -2
+##print(res)
+##print(res1)
+
+#bol6
+
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##res = a < b < c 
+##print(res)
+
+#bol7
+
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##res = a < b and b < c 
+##print(res)
+
+#bol8
+
+##a=int(input())
+##b=int(input())
+##res = b % 2 > 0 and a % 2 > 0 
+##print(res)
+
+#bol9
+
+##a=int(input())
+##b=int(input())
+##res = b % 2 > 0 or a % 2 > 0 
+##print(res)
+
+#bol10
+
+##a=int(input())
+##b=int(input())
+##res = b % 2 > 0 and a % 2 <= 0 or b % 2 <= 0 and a % 2 > 0
+##print(res)
+
+#bol11
+
+##a=int(input())
+##b=int(input())
+##res = b % 2 > 0 and a % 2 > 0 or b % 2 == 0 and a % 2 == 0
+##print(res)
+
+#bol12
+
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##res = a % 2 == 0 and b % 2 == 0 and c % 2 == 0 
+##print(res)
+
+#bol13
+
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##res = b % 2 == 0 or a % 2 == 0 or c % 2 == 0
+##print(res)
+
+#bol14
+
+##a=int(input())
+##b=int(input())
+##c=int(input())
+##res = b > 0 and a < 0 and c < 0 or b < 0 and a > 0 and c < 0 or b < 0 and a < 0 and c > 0
+##print(res)
+
